83af6a123fc017d4b46387e41bf5f2910cd95186
Yun.Lei <leiy@huizhuang.com>
2019-04-26 14:34:26 +0800

    model change

--- src/store/modules/Model.js
+++ src/store/modules/Model.js
@@ -2,7 +2,7 @@
  * @Author: Yun.Lei
  * @Date: 2019-04-25 16:33:11
  * @Last Modified by: Yun.Lei
- * @Last Modified time: 2019-04-25 18:23:37
+ * @Last Modified time: 2019-04-26 14:34:00
  */
 "use strict";
 import cookie from "js-cookie";
@@ -10,31 +10,16 @@ import Api from "@api/Api";
 
 //state
 const state = {
-    sidebar: {
-        opened: !+cookie.get("sidebarStatus"),
-        withoutAnimation: false
-    },
-    device: "desktop",
+    
 };
 
 //getters
 const getters = {
-    getSidebar: state => {
-        return state.sidebar;
-    },
+    
 };
 
 //actions
 const actions = {
-    CloseSideBar({ commit }, { withoutAnimation }) {
-        commit("CLOSE_SIDEBAR", withoutAnimation);
-    },
-    ToggleDevice({ commit }, device) {
-        commit("TOGGLE_DEVICE", device);
-    },
-    ToggleSideBar: ({ commit }) => {
-        commit("TOGGLE_SIDEBAR");
-    },
     getArticleList({}, page) {
         return new Promise((resolve, reject) => {
             Api.IGetArticleList(
@@ -65,23 +50,7 @@ const actions = {
 
 //mutations
 const mutations = {
-    CLOSE_SIDEBAR: (state, withoutAnimation) => {
-        cookie.set("sidebarStatus", 1);
-        state.sidebar.opened = false;
-        state.sidebar.withoutAnimation = withoutAnimation;
-    },
-    TOGGLE_DEVICE: (state, device) => {
-        state.device = device;
-    },
-    TOGGLE_SIDEBAR: state => {
-        if (state.sidebar.opened) {
-            cookie.set("sidebarStatus", 1);
-        } else {
-            cookie.set("sidebarStatus", 0);
-        }
-        state.sidebar.opened = !state.sidebar.opened;
-        state.sidebar.withoutAnimation = false;
-    },
+    
 };
 
 export default {
